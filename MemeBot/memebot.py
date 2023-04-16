37e9335643700af96b94eb613c088d8041039ba5
Breadsauce <nick.b.bradshaw@gmail.com>
2023-04-16 17:47:13 -0500

    Create memebot.py

---
+++ MemeBot/memebot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import asyncpraw
+import asyncio
+import random
+
+# Create a new Discord client
+intents = discord.Intents().all()
+intents.members = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    # meme me daddy
+    reddit = asyncpraw.Reddit(
+        client_id="REDDIT ID GOES HERE",
+        client_secret="REDDIT SECRECT GOES HERE",
+        user_agent="Discord_bot",
+        aiohttp_kwargs={"timeout": 10},
+    )
+    list = []
+    subreddit = await reddit.subreddit("memes")
+    async for submission in subreddit.hot(limit=50):
+            list.append(submission)
+    await message.channel.send(random.choice(list).url)
+
+# Run the bot
+client.run('DISCORD TOKEN GOES HERE')
